f309c26459af99b4dc0a06f1de4338425a66f7e4
Kowsalya-t <65986161+Kowsalya-t@users.noreply.github.com>
2020-10-08 13:21:29 +1100

    Create report_usage_inter.sql

---
+++ report_usage_inter.sql
@@ -0,0 +1,27 @@
+CREATE EXTERNAL TABLE report_usage_inter(
+  `applicationname` string,
+  `userid` string,
+  `id` string,
+  `sourcename` string,
+  `reportname` string,
+  `description` string,
+  `timezonename` string,
+  `submittime` string,
+  `state` string,
+  `lastinfo` string,
+  `emails` string,
+  `stackname` string,
+  `currentdatetime` string
+  )
+ROW FORMAT SERDE
+'org.apache.hadoop.hive.serde2.OpenCSVSerde' 
+WITH SERDEPROPERTIES (
+   'separatorChar' = ',',
+   'quoteChar' = '"'
+   )
+STORED AS TEXTFILE
+LOCATION
+  's3://mtdata-datalake-qa/report_usage_inter/'
+TBLPROPERTIES (
+  'skip.header.line.count'='1'
+  );
